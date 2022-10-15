44725f1378495d7acf4c86e88948bd7dbe1fd50c
Dayvson Silva <userddssilva@gmail.com>
2022-10-15 10:23:43 -0400

    refact: remove free word from routes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
     // like '/berry-material-react/react/default'
-    basename: '/free',
+    basename: '/',
     defaultPath: '/dashboard/default',
     fontFamily: `'Roboto', sans-serif`,
     borderRadius: 12
