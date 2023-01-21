7e255ec99707c8c73b01d23e7f2783c90ce03a38
Shah <24467345+shahx95@users.noreply.github.com>
2023-01-21 14:25:54 +0500

    Remove repetitive use of darkTitle prop (#52)
    Remove the repetitive use darkTitle prop and instead use the title prop to pass in a Typography component with the variant of "h3" to achieve the same effect. This simplifies the code and makes it more readable. 
    
    Updated code uses a ternary operator to check the value of darkTitle and conditionally render the title prop with or without the Typography component.

--- src/ui-component/cards/MainCard.js
+++ src/ui-component/cards/MainCard.js
@@ -46,10 +46,9 @@ const MainCard = forwardRef(
                 }}
             >
                 {/* card header and action */}
-                {!darkTitle && title && <CardHeader sx={headerSX} title={title} action={secondary} />}
-                {darkTitle && title && (
-                    <CardHeader sx={headerSX} title={<Typography variant="h3">{title}</Typography>} action={secondary} />
-                )}
+                {title && 
+                    ( <CardHeader sx={headerSX} title={ darkTitle? <Typography variant="h3">{title}</Typography> : title } action={secondary} /> )
+                } 
 
                 {/* content & header divider */}
                 {title && <Divider />}
