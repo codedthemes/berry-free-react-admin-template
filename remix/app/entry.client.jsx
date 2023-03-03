66af8fb9141745001315083b7fafabde9c4766cf
Harshad <codedthemes@gmail.com>
2023-03-03 10:43:36 +0530

    local changes

--- remix/app/entry.client.jsx
+++ remix/app/entry.client.jsx
@@ -1,27 +1,62 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import * as React from "react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 import { hydrateRoot } from "react-dom/client";
-import theme from "../src/themes";
+import { RemixBrowser } from "@remix-run/react";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import ClientStyleContext from "../src/ClientStyleContext";
+import createEmotionCache from "../src/createEmotionCache";
 
-function hydrate() {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <RemixBrowser />
-        </ThemeProvider>
-      </StrictMode>
-    );
-  });
+// function hydrate() {
+
+//   startTransition(() => {
+//     const customization = useSelector((state) => state.customization);
+
+//     hydrateRoot(
+//       document,
+//       <StrictMode>
+//         <ThemeProvider theme={theme(customization)}>
+//           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+//           <CssBaseline />
+//           <RemixBrowser />
+//         </ThemeProvider>
+//       </StrictMode>
+//     );
+//   });
+// }
+
+// if (typeof requestIdleCallback === "function") {
+//   requestIdleCallback(hydrate);
+// } else {
+//   // Safari doesn't support requestIdleCallback
+//   // https://caniuse.com/requestidlecallback
+//   setTimeout(hydrate, 1);
+// }
+
+function ClientCacheProvider({ children }) {
+  const [cache, setCache] = useState(createEmotionCache());
+  const customization = useSelector((state) => state.customization);
+
+  function reset() {
+    setCache(createEmotionCache());
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>
+        <ThemeProvider theme={theme(customization)}>{children}</ThemeProvider>
+      </CacheProvider>
+    </ClientStyleContext.Provider>
+  );
 }
 
-if (typeof requestIdleCallback === "function") {
-  requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  setTimeout(hydrate, 1);
-}
+hydrateRoot(
+  <ClientCacheProvider>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <RemixBrowser />
+  </ClientCacheProvider>,
+  document
+);
--- remix/app/root.jsx
+++ remix/app/root.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import {
   Links,
   LiveReload,
@@ -5,7 +6,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch
 } from "@remix-run/react";
+import { withEmotionCache } from '@emotion/react';
+import Layout from '../src/Layout';
+import ClientStyleContext from '../src/ClientStyleContext';
+import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 export const meta = () => ({
   charset: "utf-8",
@@ -13,19 +20,105 @@ export const meta = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+const Document = withEmotionCache(({ children, title }, emotionCache) => {
+  const clientStyleData = React.useContext(ClientStyleContext);
+
+  // Only executed on client
+  useEnhancedEffect(() => {
+    // re-link sheet container
+    emotionCache.sheet.container = document.head;
+    // re-inject tags
+    const tags = emotionCache.sheet.tags;
+    emotionCache.sheet.flush();
+    tags.forEach((tag) => {
+      // eslint-disable-next-line no-underscore-dangle
+      (emotionCache.sheet)._insertTag(tag);
+    });
+    // reset cache to reapply global styles
+    clientStyleData.reset();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const theme = useTheme();
+
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <meta name="emotion-insertion-point" content="emotion-insertion-point" />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+});
+
+export default function App() {
+  return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+// https://remix.run/docs/en/v1/api/conventions#errorboundary
+export function ErrorBoundary({ error }) {
+  console.error(error);
+
+  return (
+    <Document title="Error!">
+      <Layout>
+        <div>
+          <h1>There was an error</h1>
+          <p>{error.message}</p>
+          <hr />
+          <p>Hey, developer, you should replace this with what you want your users to see.</p>
+        </div>
+      </Layout>
+    </Document>
+  );
+}
+
+// https://remix.run/docs/en/v1/api/conventions#catchboundary
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = <p>Oops! Looks like you tried to visit a page that you do not have access to.</p>;
+      break;
+    case 404:
+      message = <p>Oops! Looks like you tried to visit a page that does not exist.</p>;
+      break;
+
+    default:
+      throw new Error(caught.data || caught.statusText);
+  }
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <Layout>
+        <h1>
+          {caught.status}: {caught.statusText}
+        </h1>
+        {message}
+      </Layout>
+    </Document>
   );
 }
