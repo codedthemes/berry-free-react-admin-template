b863a1c9cd6eb5beef4a27a01e065bcf5659d06e
Alexis Sanchez - El Pecoso <alexis.sanchez@student.ie.edu>
2024-03-05 11:36:38 +0100

    Creating the first version of my landing page

---
+++ create-react-app/src/views/Landing/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const LandingPage = () => {
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h2">Welcome to My Portfolio</Typography>
+      {/* Add your portfolio value, ROI components here */}
+      {/* Add your graph component here */}
+    </Box>
+  );
+};
+
+export default LandingPage;
