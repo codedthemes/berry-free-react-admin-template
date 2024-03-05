5349a3ecf73f895de61fecd74da28ef944f1629a
Alexis Sanchez - El Pecoso <alexis.sanchez@student.ie.edu>
2024-03-05 11:04:20 +0100

    Added the loading functions for my json files

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ import datetime
 app = Flask(__name__)
 CORS(app)
 
+with open("stock_portfolio.json", "r") as file:
+    portfolio = json.load(file)
 
+with open("stock_portfolio_details.json", "r") as file:
+    portfolio_details = json.load(file)
 
 if __name__ == "__main__":
     app.run(debug=True)
