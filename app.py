a66dfdb4dbe35e99e77fcbac2c7a9ff79c4ac003
Alexis Sanchez - El Pecoso <alexis.sanchez@student.ie.edu>
2024-03-05 11:05:22 +0100

    Added the function to retrieve last closing price

--- app.py
+++ app.py
@@ -13,5 +13,21 @@ with open("stock_portfolio.json", "r") as file:
 with open("stock_portfolio_details.json", "r") as file:
     portfolio_details = json.load(file)
 
+def get_stock_final_price(symbol, api_key, base_url):
+    function = "TIME_SERIES_DAILY"
+    url = f"{base_url}function={function}&symbol={symbol}&apikey={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        time_series = data.get("Time Series (Daily)", {})
+        if time_series:
+            latest_date = max(time_series.keys())
+            final_price = float(time_series[latest_date]["4. close"])
+            return final_price
+        else:
+            return "Data not available"
+    else:
+        return "Sorry the request failed. Please try again later."
+
 if __name__ == "__main__":
     app.run(debug=True)
