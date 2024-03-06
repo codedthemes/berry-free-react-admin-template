a7de7c570cafe12c331e189b3046638312f1eb4a
Alexis Sanchez - El Pecoso <alexis.sanchez@student.ie.edu>
2024-03-06 16:14:33 +0100

    One last time

--- create-react-app/src/views/Landing/StockListItem.js
+++ create-react-app/src/views/Landing/StockListItem.js
@@ -1,9 +1,12 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import React from 'react';
 import { ListItem, ListItemText } from '@mui/material';
 
+// This component renders a list item that, when clicked, navigates to the stock details page for the given symbol
 const StockListItem = ({ symbol }) => {
   return (
+    // The ListItem component is used here with the 'button' prop to make it clickable
+    // The 'component' prop is used to render the ListItem as a 'Link', enabling internal navigation to '/stock-details/:symbol'
     <ListItem button component={Link} to={`/stock-details/${symbol}`}>
       <ListItemText primary={symbol} />
     </ListItem>
@@ -11,4 +14,3 @@ const StockListItem = ({ symbol }) => {
 };
 
 export default StockListItem;
-
